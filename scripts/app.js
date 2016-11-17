268505c61cdd55ecf30bab2a43c6797e1a4ec823
Phil Fluegel <pfluegelcx@gmail.com>
2016-11-16 19:31:46 -0600

    parallax

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,9 @@
 
 (function(){
 	
+	
+		$('.fed').css({'margin-top': $(window).scrollTop()});
+		$('.phil').css({'margin-top': $(window).scrollTop()});
 
   var $hamburger = $(".hamburger");
   $hamburger.on("click", function(e) {
@@ -21,5 +24,31 @@ $(window).resize(function(){
 		$('.coach-li').removeClass('show-li');
 	}
 })
+
+function clickScroll(className, targetClass){
+		$(className).click(function(){
+			$('html, body').animate({
+				scrollTop: ($(targetClass).offset().top-50)
+			}, 'slow');
+		});
+	}
+	
+	$(window).scroll(function(){
+		
+		var distance = $(window).scrollTop();
+		var opac = ( 1 - (distance / 400));
+		var newOpac = Math.round(opac * 10) / 10;
+		if(distance > -100){
+			$('.fed').css({'margin-top': (distance /3) + 'px'});
+			$('.phil').css({'margin-top': (distance /3) + 'px'});
+			$('.fed').css({'opacity': newOpac})
+			$('.phil').css({'opacity': newOpac})
+		}
+	});
+	
+	clickScroll('.ion-chevron-down','.info');
+	clickScroll('.info-tab','.info');
+	clickScroll('.port','#port');
+	clickScroll('.top','.header');
 	
 })();
